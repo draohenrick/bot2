b85e7586db9ee03fdd30732ff6b7703906839d1c
draohenrick <agenciablocktool@gmail.com>
2025-11-03 10:30:54 -0300

    Create package.js

---
+++ dplay/package.js
@@ -0,0 +1,15 @@
+{
+  "name": "whatsapp-bot",
+  "version": "1.0.0",
+  "description": "Bot de atendimento WhatsApp usando Venom-Bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "venom-bot": "^5.2.0",
+    "puppeteer": "^24.6.0"
+  }
+}
